18e7f81e4c5b7fd4a9e48ef0b5d4e449eea0149a
peter <peterthomaskim@gmail.com>
2022-08-04 10:04:08 -0300

    add gallery 2

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -13,8 +13,11 @@ Object.keys(path).forEach((i) => {
     list.push(i.slice(18));
     // exatamente 18 char para ler as fotos
 });
-list.forEach((i) => {
-    console.log("list=> " + i);
+var list2 = [];
+const path2 = import.meta.glob(`../public/gallery2/*`);
+Object.keys(path2).forEach((i) => {
+    list2.push(i.slice(18));
+    // exatamente 18 char para ler as fotos
 });
 
 export default function Gallery() {
@@ -43,6 +46,33 @@ export default function Gallery() {
                                     <div className="swiper_slide_gallery">
                                         <img
                                             src={"/gallery/" + t[1]}
+                                            alt="foto das roupas"
+                                            style={{ aspectRatio: 4 / 5 }}
+                                        />
+                                    </div>
+                                </a>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            </div>
+            <div className="container container_column">
+                <div className="swiper_container_gallery">
+                    <Swiper
+                        modules={[Navigation, Pagination, Scrollbar]}
+                        spaceBetween={50}
+                        slidesPerView={1}
+                        navigation
+                        pagination={{ clickable: true }}
+                        scrollbar={{ draggable: true }}
+                    >
+                        {Object.entries(list2).map((t, k) => (
+                            <SwiperSlide>
+                                {/* resolver key para cada child de list */}
+                                <a href={"/gallery2/" + t[1]}>
+                                    <div className="swiper_slide_gallery">
+                                        <img
+                                            src={"/gallery2/" + t[1]}
                                             alt="foto das roupas"
                                             style={{ aspectRatio: 4 / 5 }}
                                         />
